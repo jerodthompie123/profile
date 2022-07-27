db85932ca67957b582d34c8715eda845135376ff
jerodthompie123 <jerod.thompson-springer@hotmail.com>
2022-07-27 17:20:36 -0400

    New changes

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About(){
+    return(
+        <div>
+            <h2>About Me</h2>
+            <p>
+                My name is Jerod Thompson-Springer, I am an amateur computer programmer learning to built skills in HTML, CSS and JavaScript.
+            </p>
+        </div>
+    )
+
+}
+
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Contact(){
+    return(
+        <h2>Contact Me at 246-123-4567</h2>
+
+    )
+}
+export default Contact;
