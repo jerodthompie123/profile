ea818f55456db011c5e24665d2754d39085dca76
jerodthompie123 <jerod.thompson-springer@hotmail.com>
2022-07-27 18:39:44 -0400

    new

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ function About(){
         <div>
             <h2>About Me</h2>
             <p>
-                My name is Jerod Thompson-Springer, I am an amateur computer programmer learning to built skills in HTML, CSS and JavaScript.
+                My name is Jerod Thompson-Springer, I am an amateur computer programmer learning  skills in HTML, CSS and JavaScript.
             </p>
         </div>
     )
